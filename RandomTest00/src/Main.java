44b3e3a9f7ea107714edfd5074f4344395c4bf1a
number0000 <harryhaw@gmail.com>
2019-09-07 17:38:32 -0700

    simplied sqrt added
    it predict the possible sqrt down to decimals point. I would say pretty accurate.

--- RandomTest00/src/Main.java
+++ RandomTest00/src/Main.java
@@ -11,6 +11,7 @@
  * 					...etc.
  * 
  */
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class Main {
@@ -20,6 +21,7 @@ public class Main {
 	static int consecutive_flag = 0;
 	static int flag_consecutive_count = 0;
 	static int false_play = 0;
+	static int checkError_count = 0;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -33,37 +35,34 @@ public class Main {
 		
 		random = new Random();
 		ArrayList<Double> list_fault = new ArrayList<Double>();
-		
-		Dictionary checking_list = new Hashtable();
-		checking_list.put("1", "1");
-		checking_list.put("2", "4");
-		checking_list.put("3", "9");
-		checking_list.put("4", "16");
-		checking_list.put("5", "25");
-		checking_list.put("6", "36");
-		checking_list.put("7", "49");
-		checking_list.put("8", "64");
-		checking_list.put("9", "81");
+		DecimalFormat df = new DecimalFormat();
+		df.setMaximumFractionDigits(3);
 		
 		for(int i = 1; i < 1001; i++){
-			int output = random_double();
-			double output_sqrt = Math.sqrt(output);
+			int output = randomDouble();
+			double output_real = Math.sqrt(output);
 
-			if(digit_a(output_sqrt, 4) == false){
+			if(digitA(output_real, 4) == false){
 				false_play++;
-				list_fault.add(output_sqrt);
+				list_fault.add(output_real);
 			}
-			System.out.println(i + "))  " + "[" + output + "]  "+ output_sqrt);
+			double output_sqrt = simplifiedSqrt(output);
+			System.out.println(i + "))  " + "[" + output + "]  " +
+					df.format(output_sqrt) + "  =>  " + output_real);
 		}
 		System.out.println("flag_count....." + flag_count);
 		System.out.println("false_play....." + false_play);
-		System.out.println("flag_consecutive_count.." + flag_consecutive_count);
+		System.out.println("checkError....." + checkError_count);
+		System.out.println("flag_consecutive_count.." + flag_consecutive_count + "\n");
+//		double output_sqrt = simplifiedSqrt(1522);
+//		System.out.println(df.format(output_sqrt));
+
 	}
 	
-	public static int random_double(){
+	public static int randomDouble(){
 		Random random = new Random();
 		double input = random.nextDouble() * 100000000;
-		if( digit_a(input, 8) == false){
+		if( digitA(input, 8) == false){
 			consecutive_flag++;
 			flag_count++;
 			if(consecutive_flag > 1){
@@ -71,7 +70,7 @@ public class Main {
 			}
 			System.out.println("Flag..(" + flag_count + ").. " + 
 					consecutive_flag + "=>" + input);
-			return random_double();
+			return randomDouble();
 		}
 		consecutive_flag = 0;
 		
@@ -79,25 +78,56 @@ public class Main {
 		
 	}
 	
-	public static boolean digit_a(double input, int a){
+	public static boolean digitA(double input, int a){
 		int a_input = 1;
 		for(int i = 1; i <= a-1; i++) a_input *= 10;
 		if(input > a_input && input < (a_input * 10))	return true;
 		else return false;
 	}
 	
-	public static int simplified_sqrt(int input, Dictionary checking_list){
-		int n = checking_list.size();
+	public static double simplifiedSqrt(int input){
+		int n = 0;
+		int index = 2;
+		boolean flag = false;
+		int numerator = 0, denominator = 1;
+		double results;
 		//First part
-		for (int i = 1; i < n; i++){
-			
+		for (int i = 2; i <= input/2; i++){
+			n = squared(i);
+			index = i;
+			if (n == input){
+				flag = true;
+				break;
+			}
+			if (n > input){
+				break;
+			}
 		}
+		//Testing part
+//		System.out.println("index = " + (index-1) + " n = " + squared(index-1) + " input = " + input);
+//		System.out.println("index = " + index + " n = " + n + " input = " + input);
+
 		//Second part
+		if (flag == false){
+			numerator = input - squared(index - 1);
+			denominator = 2 * (index-1);
+			results = (index - 1) + (double) (numerator) / denominator;
+		} else {
+			numerator = 0;
+			denominator = 1;
+			results = index;
+		}
 		
-		return (int) output;
+		System.out.println(flag + " " + results + " => " + (index-1) + " " + numerator + " / "  + denominator);
+		if(checkError(Math.sqrt(input), results) == false){
+			System.out.println("Flag..(" + checkError_count + ").. " + 
+					"..real.." + Math.sqrt(input) + " hypothesis " + results);
+			checkError_count++;
+		}
+		return (double) results;
 	}
 	
-	public static int check_last_index(int input){
+	public static int checkLastIndex(int input){
 		String s = String.valueOf(input);
 		int i = s.length();
 		while(i > 0){
@@ -106,6 +136,17 @@ public class Main {
 		return Integer.parseInt(s.substring(i));
 	}
 	
+	public static int squared(int input){
+		return input * input;
+	}
+	
+	public static boolean checkError(double d, double results){
+		double error_rate = 1.0;
+		double floor_error = results - error_rate;
+		double ceiling_error = results + error_rate;
+		if (d >= floor_error && d <= ceiling_error)	return true;
+		else	return false;
+	}
 	
 	
 
